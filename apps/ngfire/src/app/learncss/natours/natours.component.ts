5a90d145337256072d5e786c6359756af50f8cd7
gilsan <jhhan001@gmail.com>
2022-04-24 13:03:34 +0900

    commit

--- apps/ngfire/src/app/learncss/natours/natours.component.ts
+++ apps/ngfire/src/app/learncss/natours/natours.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-natours',
@@ -7,13 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NatoursComponent implements OnInit {
 
-  constructor() { }
+  popup: boolean = false;
+  constructor(
+   private ngzone: NgZone
+  ) {}
 
   ngOnInit(): void {
   }
 
   radioButtonClick(button: string) {
       console.log(button);
+  }
+
+  toggle() {
+     this.popup = true;
+  }
+
+  styleStatus() {
+    if (this.popup) {
+      return { 'opacity': '1', 'visibility': 'visible'};
+    }
+    return { 'opacity': '0', 'visibility': 'hidden'};
+  }
+  goback(): void {
+    this.popup = false;
+  //  return '/learncss/natours';
   }
 
 }
