240d318ec1d5d7b009eca377f16b9911801cbfd1
gilsan <jhhan001@gmail.com>
2022-05-03 09:30:43 +0900

    commit

--- apps/ngfire/src/app/learncss/nexter/nexter.component.ts
+++ apps/ngfire/src/app/learncss/nexter/nexter.component.ts
@@ -7,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NexterComponent implements OnInit {
 
+  homes = [
+    {
+      svc: 'img/sprite.svg#icon-global',
+      header: 'World\'s best luxury homes',
+      text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.'},
+    {
+      svc:'img/sprite.svg#icon-trophy',
+      header: 'Only the best properties',
+      text: 'Voluptatum mollitia quae. Vero ipsum sapiente molestias accusamus rerum sed a eligendi aut quia.'
+    },
+    {
+      svc:'img/sprite.svg#icon-map-pin',
+      header: 'All homes in in top locations',
+      text: 'Tenetur distinctio necessitatibus pariatur voluptatibus quidem consequatur harum.'
+    },
+    {
+      svc:'img/sprite.svg#icon-key',
+      header: 'New home in one week',
+      text: 'Vero ipsum sapiente molestias accusamus rerum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+    {
+      svc:'img/sprite.svg#icon-presentation',
+      header: 'Top 1% realtors',
+      text: 'Quidem consequatur harum, voluptatum mollitia quae. Tenetur distinctio necessitatibus pariatur voluptatibus.'
+    },
+    {
+      svc:'img/sprite.svg#icon-lock',
+      header: 'Secure payments on nexter',
+      text: 'Pariatur voluptatibus quidem consequatur harum, voluptatum mollitia quae.'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
