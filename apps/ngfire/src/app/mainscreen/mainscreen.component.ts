346448e351c22819bad177710b633a28f0dd4a25
gilsan <jhhan001@gmail.com>
2022-08-18 16:03:58 +0900

    commit

---
+++ apps/ngfire/src/app/mainscreen/mainscreen.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+
+
+@Component({
+  selector: 'app-mainscreen',
+  templateUrl: './mainscreen.component.html',
+  styleUrls: ['./mainscreen.component.scss'],
+})
+export class SidebarComponent implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+
+
+}
