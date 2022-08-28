c41972917e68c368515f3f284aad1c754658414d
gilsan <jhhan001@gmail.com>
2022-08-28 11:43:52 +0900

    commit

--- apps/ngfire/src/app/css-animation/components/angularAnimations/angularBasic/angularAnimation.component.ts
+++ apps/ngfire/src/app/css-animation/components/angularAnimations/angularBasic/angularAnimation.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate} from '@angular/animations';
-import { barState, blinkState, numberEntereState, widthState } from './animations';
+import { barState, blinkState, boxState, numberEntereState, widthState,  } from './animations';
 import { interval, Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { SubSink } from 'subsink';
+import { timeStamp } from 'console';
 @Component({
   selector: 'app-animation',
   templateUrl: './angularAnimation.component.html',
   styleUrls: ['./angularAnimation.component.scss'],
   animations: [
     numberEntereState, blinkState,
-    barState, widthState
+    barState, widthState,boxState,
 
   ]
 
@@ -18,6 +19,15 @@ import { SubSink } from 'subsink';
 export class AngularAnimationComponent implements OnInit, OnDestroy {
 
   stateBlink = 'show';
+  loadingBorder = 'start';
+  movingBar = "start";
+  loadingBox = true;
+  makecircle = 'circle';
+  makecircleState = false;
+
+  imagestate =false;
+  loadingbackground = false;
+
   width = {};
   counter = 0;
   progressBarFinish = false;
@@ -31,13 +41,25 @@ export class AngularAnimationComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.blink();
-    this.progressBar()
+    this.progressBar();
+    this.loadingborder();
+
   }
 
   blink() {
     this.subs.sink = interval(1000).subscribe(val => {
       this.stateBlink = this.stateBlink === 'show' ? 'hide' : 'show';
     });
+  }
+
+  loadingborder() {
+
+    this.subs.sink = interval(1000).pipe(
+        take(2)
+    ).subscribe(val => {
+      this.loadingBorder ='finish';
+    })
+
   }
 
   progressBar() {
@@ -49,14 +71,46 @@ export class AngularAnimationComponent implements OnInit, OnDestroy {
 
       if (val >= 100) {
         this.progressBarFinish = true;
+        this.movingbar();
       }
     })
 
   }
 
+  movingbar() {
+    this.subs.sink = interval(300).pipe(take(21)).subscribe(val => {
+      if (val === 0) {
+        this.movingBar = 'finish';
+      } else if (val === 2) {
+        this.loadingBox = false;
+      } else if (val === 3) {
+        this.makeCircle();
+      } else if (val === 18) {
+        this.hideCircle();
+      } else if (val === 19) {
+        this.imageState();
+      } else if (val === 20) {
+        this.loadingBackground();
+      }
+    })
 
+  }
 
+  makeCircle() {
+    this.makecircle = 'circle';
+  }
 
+  hideCircle() {
+   this.makecircle = 'hide'
+  }
+
+  imageState() {
+    this.imagestate = true;
+  }
+
+  loadingBackground() {
+    this.loadingbackground = true;
+  }
 
 
 
--- apps/ngfire/src/app/css-animation/components/angularAnimations/angularBasic/animations.ts
+++ apps/ngfire/src/app/css-animation/components/angularAnimations/angularBasic/animations.ts
@@ -1,5 +1,5 @@
 
-import { trigger, state, style, transition, animate} from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes} from '@angular/animations';
 
 export const numberEntereState =  trigger('numberEntereeState', [
   state('unselectedNumber', style({
@@ -37,39 +37,68 @@ export const blinkState = trigger('blink', [
     ]
 )
 
-export const widthState = trigger('width', [
-  transition('* => *', animate(1000, style({
-    width: '85%'
-  }))),
-  transition('*=> *', animate(1000, style({
-    width: '200px'
-  })) )
+export const widthState = trigger('linewidth', [
+  state('start', style({
+    width: '85%',
+    height: '1px',
+    background: 'red',
+    position: 'absolute',
+    bottom: '0',
+    left: '50%',
+    transform: 'translateX(-50%)',
+  })),
+  state('finish', style({
+    width: '200px',
+    height: '1px',
+    background: '#fff',
+    position: 'absolute',
+    bottom: '0',
+    left: '50%',
+    transform: 'translateX(-50%)',
+     })),
+  transition('start => finish', animate(3000)),
 ])
 
 
 export const barState = trigger('movingBar', [
+   state('start', style({
+    height: '10px',
+    width: '90%',
+    borderRadius: '2px',
+    background: '#6cff8d',
+   })),
+   state('finish', style({
+    background: '#6cff8d',
+    rotate: '270deg',
+    top: '200%',
+    left: '1000%'
 
+   })),
+  transition('start => finish',   animate(3000))
+])
 
+export const boxState = trigger('makeCircle', [
+   state('circle', style({
+    height: '300px',
+    borderRadius: '50%'
+   })),
+   state('hide', style({
+     border: 'none'
+   })),
+   transition('void => circle', [
+    animate(4000),
+   ]),
 
-  //  state('end', style({
-  //   height: '10px',
-  //   width: '90%',
-  //   borderRadius: '2px',
-  //   background: '#6cff8d',
-  //   rotate: '90deg',
-  //   top: '200%',
-  //   left: '1000%'
+])
 
-  //  })),
-  //  transition('void => *', animate(2000) ),
-  transition('void => *',   [
-    style({
-      height: '10px',
-      width: '90%',
-      borderRadius: '2px',
-      background: '#6cff8d',
-    }),
-    animate(2000)
-  ]),
-  // transition('void => end',   animate(1000))
+export const imageState = trigger('rolling', [
+    state('start', style({
+      transform: 'rotate(0deg) translate(-50%, -50%)',
+      transformOrigin: 'center'
+    })),
+    state('end', style({
+      transform: 'rotate(360deg) translate(-50%, -50%)',
+      transformOrigin: 'center'
+    })),
+    transition('start => end', animate(3000))
 ])
